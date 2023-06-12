caec7c178646fbdc4e7dca174a31d2b48e513a32
carlossg88 <carlossg88@hotmail.com>
2023-06-12 08:16:28 -0500

    Adding images to landscapes

--- main.js
+++ main.js
@@ -6,4 +6,24 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
   MobileMenu.classList.toggle('inactive')
+  
+}
+
+// Listen the click to open images
+const See = document.querySelector('.See');
+See.addEventListener('click', openFullImg);
+
+const FullImgBox = document.getElementById("fullImgBox");
+fullImg = document.getElementById("fullImg");
+
+
+function openFullImg(){
+  console.log('click');
+  FullImgBox.style.display = "flex";
+  fullImg.src = './images/hike1.jpg'
+  
+}
+
+function closeImg(){
+  FullImgBox.style.display = "none";
 }
